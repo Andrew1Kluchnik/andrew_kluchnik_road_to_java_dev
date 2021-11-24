d636d32a21b751bd4a76940b2b54126e8356168b
Sinitsyn Rodyslav <r.sinitsyn@elsevier.com>
2021-11-24 15:52:14 +0200

    01_types: Add task about base types int/float/arr

---
+++ src/main/java/com/andrew/types/TypeUtilsImpl.java
@@ -0,0 +1,43 @@
+package com.andrew.types;
+
+public class TypeUtilsImpl implements TypeUtils{
+    @Override
+    public int[] sort(int[] array) {
+        return new int[0];
+    }
+
+    @Override
+    public float arithmeticMean(int[] array) {
+        return 0;
+    }
+
+    @Override
+    public float max(int[] array) {
+        return 0;
+    }
+
+    @Override
+    public float min(int[] array) {
+        return 0;
+    }
+
+    @Override
+    public String plus(String value1, String value2) {
+        return null;
+    }
+
+    @Override
+    public String minus(String value1, String value2) {
+        return null;
+    }
+
+    @Override
+    public String mul(String value1, String value2) {
+        return null;
+    }
+
+    @Override
+    public String div(String value1, String value2) {
+        return null;
+    }
+}
