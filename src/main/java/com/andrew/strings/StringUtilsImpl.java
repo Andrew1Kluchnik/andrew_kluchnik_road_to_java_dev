1f85f75e2894a0d31223f52f23ac4eb16fc937a8
Sinitsyn Rodyslav <r.sinitsyn@elsevier.com>
2021-11-25 10:38:57 +0200

    Add StringUtils to implement.

---
+++ src/main/java/com/andrew/strings/StringUtilsImpl.java
@@ -0,0 +1,33 @@
+package com.andrew.strings;
+
+public class StringUtilsImpl implements StringUtils{
+    @Override
+    public int charCount(String str, char letter) {
+        return 0;
+    }
+
+    @Override
+    public String reverse(String str) {
+        return null;
+    }
+
+    @Override
+    public StringBuilder alphabet() {
+        return null;
+    }
+
+    @Override
+    public String toCamelCase(String str) {
+        return null;
+    }
+
+    @Override
+    public String fromCamelCase(String camelStr) {
+        return null;
+    }
+
+    @Override
+    public String trim(String str) {
+        return null;
+    }
+}
