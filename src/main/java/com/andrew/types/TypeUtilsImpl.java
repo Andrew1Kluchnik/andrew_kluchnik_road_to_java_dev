a9062557a3933620c0d45692b5a912d5d133dd78
andre <zozo1901@mail.ru>
2021-11-25 09:23:49 +0200

    Questions about tests

--- src/main/java/com/andrew/types/TypeUtilsImpl.java
+++ src/main/java/com/andrew/types/TypeUtilsImpl.java
@@ -1,43 +1,87 @@
 package com.andrew.types;
 
-public class TypeUtilsImpl implements TypeUtils{
-    @Override
+public class TypeUtilsImpl implements TypeUtils {
     public int[] sort(int[] array) {
-        return new int[0];
+        int[] backArray = array;
+
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (backArray[j] > backArray[j + 1]) {
+                    int numb = backArray[j];
+                    backArray[j] = backArray[j + 1];
+                    backArray[j + 1] = numb;
+                }
+            }
+        }
+        return backArray;
     }
 
     @Override
     public float arithmeticMean(int[] array) {
-        return 0;
+        float result = 0;
+        for (int i = 0; i < array.length; i++) {
+            result += array[i];
+        }
+        result = result / array.length;
+        return result;
+
     }
 
     @Override
     public float max(int[] array) {
-        return 0;
+        float maxArray = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maxArray) {
+                maxArray = array[i];
+            }
+        }
+
+        return maxArray;
     }
 
     @Override
     public float min(int[] array) {
-        return 0;
+
+        float minArray = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < minArray) {
+                minArray = array[i];
+            }
+        }
+
+        return minArray;
     }
 
     @Override
     public String plus(String value1, String value2) {
-        return null;
+        int val1 = Integer.parseInt(value1);
+        int val2 = Integer.parseInt(value2);
+        return String.valueOf(val1 + val2);
     }
 
     @Override
     public String minus(String value1, String value2) {
-        return null;
+        int val1 = Integer.parseInt(value1);
+        int val2 = Integer.parseInt(value2);
+        return String.valueOf(val1 - val2);
     }
 
     @Override
     public String mul(String value1, String value2) {
-        return null;
+        int val1 = Integer.parseInt(value1);
+        int val2 = Integer.parseInt(value2);
+        return String.valueOf(val1 * val2);
+
     }
 
     @Override
     public String div(String value1, String value2) {
-        return null;
+
+        int val1 = Integer.parseInt(value1);
+        int val2 = Integer.parseInt(value2);
+        if (val2 != 0) {
+            return String.valueOf(val1 / val2);
+        }
+        return "Divide on zero not allowed";
     }
 }
