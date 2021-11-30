9842507332c85f09148c7591732d0a17962e692a
andre <zozo1901@mail.ru>
2021-11-30 16:11:55 +0200

    Ok, i'd done it

--- src/main/java/com/andrew/strings/StringUtilsImpl.java
+++ src/main/java/com/andrew/strings/StringUtilsImpl.java
@@ -1,6 +1,6 @@
 package com.andrew.strings;
 
-import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 public class StringUtilsImpl implements StringUtils {
     @Override
@@ -17,11 +17,11 @@ public class StringUtilsImpl implements StringUtils {
 
     @Override
     public String reverse(String str) {
-       char [] arrayStr = str.toCharArray();
-        for (int i = 0; i < arrayStr.length/2; i++) {
+        char[] arrayStr = str.toCharArray();
+        for (int i = 0; i < arrayStr.length / 2; i++) {
             char letter = arrayStr[i];
-            arrayStr[i] = arrayStr[arrayStr.length-i-1];
-            arrayStr[arrayStr.length-i-1] = letter;
+            arrayStr[i] = arrayStr[arrayStr.length - i - 1];
+            arrayStr[arrayStr.length - i - 1] = letter;
         }
         String string = new String(arrayStr);
         return string;
@@ -29,21 +29,60 @@ public class StringUtilsImpl implements StringUtils {
 
     @Override
     public StringBuilder alphabet() {
-        return null;
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for (char i = 'a'; i <= 'z'; i++) {
+            if (count % 2 == 0) {
+                sb.append(Character.toUpperCase(i));
+                count++;
+            } else {
+                sb.append(i);
+                count++;
+            }
+        }
+        return sb;
     }
 
     @Override
     public String toCamelCase(String str) {
-        return null;
+        str = str.trim().replaceAll(" +", " ").replaceAll(",", "");
+        char[] chars = str.toCharArray();
+        String result = "";
+        for (int i = 0; i < chars.length; i++) {
+            if (i == 0 || Character.toString(chars[i - 1]).equals(" ")) {
+                result += Character.toUpperCase(chars[i]);
+            } else result += Character.toLowerCase(chars[i]);
+        }
+        result = result.replaceAll(" ","");
+
+        return result;
+
     }
 
     @Override
     public String fromCamelCase(String camelStr) {
-        return null;
+        char[] arrayOfString = camelStr.toCharArray();
+        String result = "";
+        ArrayList arrayList = new ArrayList();
+        for (int i = 0; i < arrayOfString.length; i++) {
+            if (Character.isUpperCase(arrayOfString[i]) && i > 1) {
+                result += " ";
+                result += Character.toLowerCase(arrayOfString[i]);
+            } else {
+                result += arrayOfString[i];
+            }
+        }
+
+        return result;
     }
 
     @Override
     public String trim(String str) {
-        return null;
+        String result = str;
+        while (result.contains("  ")) {
+            result = result.trim().replaceAll(" +", " ");
+        }
+
+        return result;
     }
 }
